018d3ef757b7c81a11da2a9b5ed93b8555c1211b
Djedjemel Arnauld Cyriaque <arnauld-cyriaque.djedjemel@etudiant.univ-rennes1.fr>
2021-10-19 15:58:42 +0200

    class implementation

--- src/istic/aco/editor/EngineImpl.java
+++ src/istic/aco/editor/EngineImpl.java
@@ -1,67 +1,125 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
-package istic.aco.editor;
+package aco_v1;
 
 public class EngineImpl implements Engine {
-    private StringBuilder buffer;
-    private String clipboard;
-    private Selection selection;
+	private StringBuilder buffer;
+	private String clipboard;
+	private Selection selection;
+	
+	/**
+	 * Constructeur par defaut
+	 */
+	public EngineImpl() {
+	}
+    /**
+     * Constructeur initialisant les  paramètres
+     * @param buffer
+     * @param clipboard
+     */
+	public EngineImpl(StringBuilder buffer,Selection s) {
+		this.buffer = buffer;
+		this.selection=s;
+	}
 
-    public EngineImpl() {
-    }
+	public StringBuilder getBuffer() {
+		return buffer;
+	}
 
-    public EngineImpl(StringBuilder buffer, Selection s) {
-        this.buffer = buffer;
-        this.selection = s;
-    }
+	public void setBuffer(StringBuilder buffer) {
+		this.buffer = buffer;
+	}
 
-    public StringBuilder getBuffer() {
-        return this.buffer;
-    }
-
-    public void setBuffer(StringBuilder buffer) {
-        this.buffer = buffer;
-    }
-
+	/**
+     * Provides access to the selection control object
+     *
+     * @return the selection object
+     */
+    @Override
     public Selection getSelection() {
+        // TODO
         return this.selection;
     }
-
+    /**
+     * recuperer le contenu de la selection
+     * @return
+     */
     public String getSelectionContents() {
-        return this.buffer.substring(this.selection.getBeginIndex(), this.selection.getEndIndex() + 1);
+    	return this.buffer.substring(this.selection.getBeginIndex(), this.selection.getEndIndex()+1);
     }
 
+    /**
+     * Provides the whole contents of the buffer, as a string
+     *
+     * @return a copy of the buffer's contents
+     */
+    @Override
     public String getBufferContents() {
+        // TODO
         return this.buffer.toString();
     }
 
+    /**
+     * Provides the clipboard contents
+     *
+     * @return a copy of the clipboard's contents
+     */
+    @Override
     public String getClipboardContents() {
+        // TODO
         return this.clipboard;
     }
 
+    /**
+     * Removes the text within the interval
+     * specified by the selection control object,
+     * from the buffer.
+     */
+    @Override
     public void cutSelectedText() {
-        String s = this.buffer.substring(this.selection.getBeginIndex(), this.selection.getEndIndex() + 1);
-        this.clipboard = s;
-        this.buffer.delete(this.selection.getBeginIndex(), this.selection.getBeginIndex());
+        // TODO
+    	String s= this.buffer.substring(this.selection.getBeginIndex(), this.selection.getEndIndex()+1);
+    	this.clipboard=s;
+    	this.buffer.delete(this.selection.getBeginIndex(), this.selection.getBeginIndex());
     }
 
+    /**
+     * Copies the text within the interval
+     * specified by the selection control object
+     * into the clipboard.
+     */
+    @Override
     public void copySelectedText() {
-        String s = this.buffer.substring(this.selection.getBeginIndex(), this.selection.getEndIndex() + 1);
-        this.clipboard = s;
+        // TODO
+    	String s= this.buffer.substring(this.selection.getBeginIndex(), this.selection.getEndIndex()+1);
+    	this.clipboard=s;
     }
 
+    /**
+     * Replaces the text within the interval specified by the selection object with
+     * the contents of the clipboard.
+     */
+    @Override
     public void pasteClipboard() {
-        this.buffer.replace(this.selection.getBeginIndex(), this.selection.getEndIndex(), this.clipboard);
+        // TODO
+    	this.buffer.replace(this.selection.getBeginIndex(), this.selection.getEndIndex(), clipboard);
     }
 
+    /**
+     * Inserts a string in the buffer, which replaces the contents of the selection
+     *
+     * @param s the text to insert
+     */
+    @Override
     public void insert(String s) {
-        this.buffer.replace(this.selection.getBeginIndex(), this.selection.getEndIndex(), s);
+    	this.buffer.replace(this.selection.getBeginIndex(), this.selection.getEndIndex(), s);
     }
 
+    /**
+     * Removes the contents of the selection in the buffer
+     */
+    @Override
     public void delete() {
-        this.buffer.delete(this.selection.getBeginIndex(), this.selection.getBeginIndex());
+    	this.buffer.delete(this.selection.getBeginIndex(), this.selection.getBeginIndex());
     }
+
+
 }
--- src/istic/aco/editor/SelectionImpl.java
+++ src/istic/aco/editor/SelectionImpl.java
@@ -1,50 +1,67 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
-package istic.aco.editor;
+package aco_v1;
 
 public class SelectionImpl implements Selection {
     private int beginIndex;
     private int endIndex;
     private StringBuilder buffer;
-    static int BUFFER_BEGIN_INDEX = 0;
+     static int BUFFER_BEGIN_INDEX = 0;
+    
+	/**
+	 * @param beginIndex
+	 * @param endIndex
+	 * @param buffer
+	 */
+	public SelectionImpl(int beginIndex, int endIndex, StringBuilder buffer) {
+		if(beginIndex<=endIndex && endIndex<=buffer.length()-1 && beginIndex>=0) {
+			this.beginIndex = beginIndex;
+			this.endIndex = endIndex;
+			this.buffer = buffer;
+		}
+	}
 
-    public SelectionImpl(int beginIndex, int endIndex, StringBuilder buffer) {
-        if (beginIndex <= endIndex && endIndex <= buffer.length() - 1 && beginIndex >= 0) {
-            this.beginIndex = beginIndex;
-            this.endIndex = endIndex;
-            this.buffer = buffer;
-        }
+	/**
+	 * @param buffer the buffer to set
+	 */
+	public void setBuffer(StringBuilder buffer) {
+		this.buffer = buffer;
+	}
 
-    }
 
-    public void setBuffer(StringBuilder buffer) {
-        this.buffer = buffer;
-    }
+	@Override
+	public int getBeginIndex() {
+		// TODO Auto-generated method stub
+		return this.beginIndex;
+	}
 
-    public int getBeginIndex() {
-        return this.beginIndex;
-    }
+	@Override
+	public int getEndIndex() {
+		// TODO Auto-generated method stub
+		return this.endIndex;
+	}
 
-    public int getEndIndex() {
-        return this.endIndex;
-    }
+	@Override
+	public int getBufferBeginIndex() {
+		// TODO Auto-generated method stub
+		return BUFFER_BEGIN_INDEX;
+	}
 
-    public int getBufferBeginIndex() {
-        return BUFFER_BEGIN_INDEX;
-    }
+	@Override
+	public int getBufferEndIndex() {
+		// TODO Auto-generated method stub
+		return this.buffer.length()-1;
+	}
 
-    public int getBufferEndIndex() {
-        return this.buffer.length() - 1;
-    }
+	@Override
+	public void setBeginIndex(int beginIndex) {
+		// TODO Auto-generated method stub
+		this.beginIndex=beginIndex;
+		
+	}
 
-    public void setBeginIndex(int beginIndex) {
-        this.beginIndex = beginIndex;
-    }
+	@Override
+	public void setEndIndex(int endIndex) {
+		// TODO Auto-generated method stub
+		this.endIndex= endIndex;
+	}
 
-    public void setEndIndex(int endIndex) {
-        this.endIndex = endIndex;
-    }
 }
