9297b1f833f47543545f53395de7b1ff93ac8e83
Djedjemel Arnauld Cyriaque <arnauld-cyriaque.djedjemel@etudiant.univ-rennes1.fr>
2021-11-03 22:52:24 +0100

    javadoc commentaries blocks add for version 1

--- src/test/java/editor/SelectionImplTest.java
+++ src/test/java/editor/SelectionImplTest.java
@@ -1,13 +1,19 @@
-package editor;
+package test.java.editor;
 
-import istic.aco.editor.SelectionImpl;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import main.java.istic.aco.editor.SelectionImpl;
+/**
+ * SelectionImplTest for Selection test
+ * @author Arnauld Djedjemel
+ * @author Dieu-Donn Padonou
+ *
+ */
 class SelectionImplTest {
 
     StringBuilder testBf;
@@ -15,7 +21,7 @@ class SelectionImplTest {
 
     @BeforeEach
     void initializer(){
-        testBf = new StringBuilder("Buffer crÃ©er pour faire des test sur les getters et getters setters.");
+        testBf = new StringBuilder("Buffer créer pour faire des test sur les getters et getters setters.");
         sp = new SelectionImpl(2, 12, testBf);
     }
 
@@ -23,7 +29,7 @@ class SelectionImplTest {
     @DisplayName("Begin index should be lower than end index")
     void beginIndexShouldBeLowerThanEndIndex() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> new SelectionImpl(27, 8, testBf));
-        String msg = "L'index de fin doit Ãªtre supÃ©rieure ou Ã©gale Ã  l'index de dÃ©but.";
+        String msg = "L'index de fin doit être supérieure ou égale à l'index de début.";
         assert (msg.equals(exception.getMessage()));
     }
 
@@ -31,14 +37,14 @@ class SelectionImplTest {
     @DisplayName("Begin index should be greater or equals to 0")
     void beginIndexShouldBeGreaterThan0() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> new SelectionImpl(-6, 8, testBf));
-        String msg = "L'index de dÃ©but doit Ãªtre supÃ©rieure ou Ã©gale Ã  0.";
+        String msg = "L'index de début doit être supérieure ou égale à 0.";
         assert (msg.equals(exception.getMessage()));
     }
 
     @Test
     @DisplayName("End index should be greater 0")
     void EndIndexShouldBeGreaterThan0() {
-        Assertions.assertDoesNotThrow(() -> new SelectionImpl(0, 0, testBf), "L'index de fin doit Ãªtre supÃ©rieure ou Ã©gale Ã  0.");
+        Assertions.assertDoesNotThrow(() -> new SelectionImpl(0, 0, testBf), "L'index de fin doit être supérieure ou égale à 0.");
     }
 
     @Test
@@ -83,7 +89,7 @@ class SelectionImplTest {
     @DisplayName("Can't set a begin index lower than 0")
     void setBeginIndex1() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> sp.setBeginIndex(-3));
-        String msg = "L'index de dÃ©but doit Ãªtre supÃ©rieure ou Ã©gale Ã  0.";
+        String msg = "L'index de début doit être supérieure ou égale à 0.";
         assert (msg.equals(exception.getMessage()));
     }
 
@@ -91,7 +97,7 @@ class SelectionImplTest {
     @DisplayName("Can't set a begin index greater than the end index")
     void setBeginIndex2() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> sp.setBeginIndex(39));
-        String msg = "L'index de dÃ©but doit Ãªtre infÃ©rieure ou Ã©gale Ã  l'index de fin.";
+        String msg = "L'index de début doit être inférieure ou égale à l'index de fin.";
         assert (msg.equals(exception.getMessage()));
     }
 
@@ -99,7 +105,7 @@ class SelectionImplTest {
     @DisplayName("Can't set a end index greater than the buffer length")
     void setBeginIndex3() {
         Exception exception = Assertions.assertThrows(IndexOutOfBoundsException.class, () -> sp.setBeginIndex(73));
-        String msg = "L'index de dÃ©but est hors du buffer.";
+        String msg = "L'index de début est hors du buffer.";
         assert (msg.equals(exception.getMessage()));
     }
 
@@ -107,7 +113,7 @@ class SelectionImplTest {
     @DisplayName("Can't set a end index lower than 0")
     void setEndIndex1() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> sp.setEndIndex(-3));
-        String msg = "L'index de fin doit Ãªtre supÃ©rieure ou Ã©gale Ã  0.";
+        String msg = "L'index de fin doit être supérieure ou égale à 0.";
         assert (msg.equals(exception.getMessage()));
     }
 
@@ -115,7 +121,7 @@ class SelectionImplTest {
     @DisplayName("Can't set a end index lesser than the begin index")
     void setEndIndex2() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> sp.setEndIndex(1));
-        String msg = "L'index de fin doit Ãªtre supÃ©rieure ou Ã©gale Ã  l'index de dÃ©but.";
+        String msg = "L'index de fin doit être supérieure ou égale à l'index de début.";
         assert (msg.equals(exception.getMessage()));
     }
 
